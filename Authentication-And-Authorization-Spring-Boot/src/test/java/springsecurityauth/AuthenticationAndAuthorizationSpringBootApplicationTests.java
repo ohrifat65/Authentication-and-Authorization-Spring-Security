118190ff648701ae57afc3008a66e3883d45d7f7
Oh Rifat <60514802+ohrifat65@users.noreply.github.com>
2023-12-19 21:08:38 +0600

    Add files via upload
    Create Spring boot Maven.

---
+++ Authentication-And-Authorization-Spring-Boot/src/test/java/springsecurityauth/AuthenticationAndAuthorizationSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package springsecurityauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAndAuthorizationSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
